86b0e10a4230433a6298e7d0037b68f9c54dee9e
Syed Azhar Hussain Quadri <syed.azh@northeastern.edu>
2023-02-19 10:48:03 +0530

    first push

---
+++ LAB2/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/saad/azhar/LAB2/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
