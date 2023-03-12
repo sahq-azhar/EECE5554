1bc2383099aa051fefe232f284caa5a7a0bcfd10
Syed Azhar Hussain Quadri <syed.azh@northeastern.edu>
2023-03-12 10:38:55 -0400

    Delete CTestTestfile.cmake

--- LAB 3/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/saad/azhar/LAB3/src
-# Build directory: /home/saad/azhar/LAB3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("imu_driver")
