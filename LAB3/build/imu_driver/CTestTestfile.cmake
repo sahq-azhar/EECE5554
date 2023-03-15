91a71f337d76e1d01428bc405000135c52926e77
Syed Azhar Hussain Quadri <syed.azh@northeastern.edu>
2023-03-15 16:55:28 -0400

    deleted devel and src

--- LAB3/build/imu_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/saad/azhar/LAB3/src/imu_driver
-# Build directory: /home/saad/azhar/LAB3/build/imu_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- LAB3/build/imu_driver/catkin_generated/imu_driver-msg-extras.cmake.installspace.in
+++
@@ -1,2 +0,0 @@
-set(imu_driver_MESSAGE_FILES "msg/Vectornav.msg")
-set(imu_driver_SERVICE_FILES "srv/ConvertToQuaternion.srv")
